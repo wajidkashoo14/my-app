8e0396ad65e3ba86e9f3232f7687bd2ff5aa1e46
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-12-28 21:38:21 +0530

    image slider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import StarRating from './components/starSelection';
+import ImageSlider from './components/image-slider';
+// import StarRating from './components/starSelection';
 // import Accordian from './components/accordian';
 // import RandomColor from './components/randomColor';
 
 function App() {
   return (
     <div className="App">
-      {/* accordian */}
       {/* <Accordian /> */}
-      {/* randomColor */}
       {/* <RandomColor /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={'10'} />
     </div>
   );
 }
