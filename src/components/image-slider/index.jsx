862b8850e286df364e6a37018a0e95b67984ca48
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-12-28 21:38:06 +0530

    image slider

---
+++ src/components/image-slider/index.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./index.css";
+
+function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  }
+
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length : currentSlide - 1);
+  }
+
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading! Please wait..</div>;
+  }
+
+  if (error !== null) {
+    return <div>An error occured ${error}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {images && images.length > 0
+        ? images.map((image, index) => {
+            return (
+              <img
+                key={image.id}
+                src={image.download_url}
+                alt="nature"
+                className={
+                  currentSlide === index
+                    ? "current-image"
+                    : "current-image hide-current-image"
+                }
+              />
+            );
+          })
+        : null}
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
+        {images && images.length > 0
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
+}
+
+export default ImageSlider;
